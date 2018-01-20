9c6844a3c657f49262a2b1fb155868c8d8cb05f1
Devin Zhang <MeetDevin@users.noreply.github.com>
2018-01-20 20:55:33 +0800

    统一注释格式

--- model.py
+++ model.py
@@ -6,8 +6,8 @@ import tensorflow as tf
 
 class ModelOfCNN(object):
 
-    weights = {}
-    biases = {}
+    weights = None
+    biases = None
     pass
 
     # Channels of img, default channels = 3, img in RGB
@@ -32,61 +32,55 @@ class ModelOfCNN(object):
 
     def weight_variable(self, shape):
         """
-        给权值赋值，从正态分布片段中取值，标准差：0.01
-        :param dtype: 数据类型
-        :param shape: 卷积核属性
-        :return:
+        give weight a value from normal distribution
+        :param shape: represent a convolution kernel
+        :return: weight_variable
         """
         initial = tf.truncated_normal(shape=shape, stddev=0.01, dtype="float")
         return tf.Variable(initial)
 
-    # 偏置量函数
     def bias_variable(self, shape):
+        # give biases a value
         initial = tf.constant(0.1, shape=shape, dtype="float")
         return tf.Variable(initial)
 
-    # 卷积操作
     def conv2d(self, x, w, b):
+        # convolution
         x = tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding="SAME")
         x = tf.nn.bias_add(x, b)
         return tf.nn.relu(x)
 
-    # 池化操作
     def pooling(self, x):
+        # pooling
         return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                               strides=[1, 2, 2, 1], padding="SAME")
 
-    # 局部归一化
     # def norm(self, x, lsize=4):
     #     return tf.nn.lrn(x, depth_radius=lsize, bias=1, alpha=0.001 / 9.0, beta=0.75)
 
     def output_cnn(self, images, keep_prob):
         """
-        卷积神经网络模型
-        :param images: 训练用的图片
+        model of CNN
+        :param images: input
         :return: a tensor  of shape [batch_size, NUM_CLASSES]
         """
-        # 第一层
+        # convolution layer 1
         hidden_conv1 = self.conv2d(images, self.weights["w_conv1"], self.biases["b_conv1"])
         hidden_pool1 = self.pooling(hidden_conv1)
         # hidden_norm1 = self.norm(hidden_pool1)
 
-        # 第二层
+        # convolution layer 2
         hidden_conv2 = self.conv2d(hidden_pool1, self.weights["w_conv2"], self.biases["b_conv2"])
         hidden_pool2 = self.pooling(hidden_conv2)
         # hidden_norm2 = self.norm(hidden_pool2)
 
-        # 密集连接层
+        # connections
         hidden_pool2_flat = tf.reshape(hidden_pool2, [-1, self.weights["w_fc1"].get_shape().as_list()[0]])
         hidden_fc1 = tf.nn.relu(tf.matmul(hidden_pool2_flat, self.weights["w_fc1"]) + self.biases["b_fc1"])
-        # 使用 Dropout 优化方法：用一个伯努利序列(0,1随机分布) * 神经元，随机选择每一次迭代的神经元
+        # Dropout
         hidden_fc1_dropout = tf.nn.dropout(hidden_fc1, keep_prob=keep_prob)
 
-        # 输出层，没有做 softmax 回归
+        # output layer without SoftMax
         logits = tf.add(tf.matmul(hidden_fc1_dropout, self.weights["w_fc2"]), self.biases["b_fc2"])
         return logits
 
-    @staticmethod
-    def output_w_b(self):
-        print(self.weights)
-        print(self.biases)
